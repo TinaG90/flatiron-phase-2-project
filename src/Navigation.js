909ed092c8aa96a016bcb8f60da073eb248c45fd
Altenia Gaines <gaines.tina90@gmail.com>
2023-07-01 13:59:39 -0700

    creates folder for styling

---
+++ src/Navigation.js
@@ -0,0 +1,10 @@
+function Navigation() {
+  return (
+    <div className="Navigation">
+      <h1>Home</h1>
+      <h1>My BookShelf</h1>
+      <h1>Logout</h1>
+    </div>
+  );
+}
+export default Navigation;
