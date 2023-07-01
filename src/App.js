74cb495e9f114cc5741cb3ead0de3db4d1511148
Altenia Gaines <gaines.tina90@gmail.com>
2023-07-01 14:28:16 -0700

    adds react router dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import Navigation from "./Navigation";
 import Home from "./Home";
 import BookShelf from "./Bookshelf";
 import Library from "./Library";
+import { useState } from "react";
+
 function App() {
+  const [page, setPage] = useState("");
+
   return (
     <div className="App">
       <header className="App-header">
